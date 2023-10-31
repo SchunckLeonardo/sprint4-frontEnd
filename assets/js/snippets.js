004996f40d9ac7d8926a72b7f40a0078e13aa122
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-10-30 22:13:06 -0300

    form modal and dropdown

---
+++ assets/js/snippets.js
@@ -0,0 +1,12 @@
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+    exampleModal.addEventListener('show.bs.modal', event => {
+        const button = event.relatedTarget
+        const recipient = button.getAttribute('data-bs-whatever')
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        modalTitle.textContent = `Enviar feedback`
+        modalBodyInput.value = recipient
+    })
+}
